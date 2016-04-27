908f7ce97db7551c9df0ab5497524d999a8deaa8
shoule <shoule@saidicaprio.local>
2016-04-27 14:33:26 +0800

    first commit

---
+++ textField回调不调用/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  textField回调不调用
+//
+//  Created by shoule on 16/4/27.
+//  Copyright © 2016年 SaiDicaprio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
+
+
+
